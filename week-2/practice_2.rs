e1d5b4b3500cdf88723ad5ce4a5212e432f5a5c0
Anthony <onyekachukwu.henry-johnson@pau.edu.ng>
2026-08-31 23:47:26 +0100

    (week_2)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,6 @@
+fn main(){
+   println!(); // prints just a
+   print!ln("hello");//prints
+   printin!("format {} arguments", "some"); //prints format some arguments
+   println!("My name is{}. I am the class rep of stream 1", "Henry-Johnson Onyekachukwu")
+  } 
